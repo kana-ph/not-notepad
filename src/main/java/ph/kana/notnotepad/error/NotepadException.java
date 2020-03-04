b53e9b3a333a69213cd970cd7dc76d4a03625334
KaNa <kana@kana.ph>
2020-03-05 00:29:44 +0800

    Update NotepadException.java

--- src/main/java/ph/kana/notnotepad/error/NotepadException.java
+++ src/main/java/ph/kana/notnotepad/error/NotepadException.java
@@ -1,8 +1,5 @@
 package ph.kana.notnotepad.error;
 
-/**
- * @author kana0011 &lt;marklynoel.alvarez@whitecloak.com&gt;
- */
 public class NotepadException extends RuntimeException {
 
     public NotepadException(String message, Throwable cause) {
